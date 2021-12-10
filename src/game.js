198f17ce3a83729f59009600ec56db01b40341d1
Ira Gilfanova <iraandjs@gmail.com>
2021-12-10 13:10:47 +0300

    add general game logic to src/game.js

---
+++ src/game.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const questionsCount = 3;
+
+const startGame = (rulesGame, getQuestionCorrectAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rulesGame);
+
+  for (let i = 0; i < questionsCount; i += 1) {
+    const [question, correctAnswer] = getQuestionCorrectAnswer();
+
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startGameEngine;
