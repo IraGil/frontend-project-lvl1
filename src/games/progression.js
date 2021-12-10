2cdd73d2f4a20c3227bdfbb2b02f8ba4178027ab
IraGil <iraandjs@gmail.com>
2021-12-10 15:21:59 +0300

    add brain-progressive

---
+++ src/games/progression.js
@@ -0,0 +1,40 @@
+import getRandomNumber from '../randomNumber.js';
+import startGameEngine from '../game.js';
+
+const rulesGame = 'What number is missing in the progression?';
+
+const generateProgression = (length, startNumber, step) => {
+  let nextNumber = startNumber;
+  const progression = [];
+
+  for (let i = 0; i < length; i += 1) {
+    progression.push(nextNumber);
+    nextNumber += step;
+  }
+
+  return progression;
+};
+
+const hideNumberInProgression = (progression, index) => {
+  const hiddenProgression = [...progression];
+  hiddenProgression[index] = '..';
+
+  return hiddenProgression;
+};
+
+const getQuestionCorrectAnswer = () => {
+  const lengthProgression = getRandomNumber(5, 15);
+  const indexHiddenNumber = getRandomNumber(0, lengthProgression - 1);
+  const startNumber = getRandomNumber(2, 50);
+  const stepProgression = getRandomNumber(2, 8);
+
+  const progression = generateProgression(lengthProgression, startNumber, stepProgression);
+  const hiddenProgression = hideNumberInProgression(progression, indexHiddenNumber);
+
+  const question = hiddenProgression.join(' ');
+  const correctAnswer = progression[indexHiddenNumber].toString();
+
+  return [question, correctAnswer];
+};
+
+export default () => startGameEngine(rulesGame, getQuestionCorrectAnswer);
